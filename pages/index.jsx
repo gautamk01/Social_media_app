a5ab39920dd4a975a921c1c09b9159c9dd49d688
Gautamk01 <l>
2023-04-07 14:16:48 +0530

    working on Backend Started, SO execited to complete it

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,8 +4,13 @@ import { FormCard } from "@/Components/hostFormcard";
 
 import { Navigation } from "@/Components/Navigation";
 import { PostCard } from "@/Components/PostCardlayout";
+import { useSession } from "@supabase/auth-helpers-react";
+import LoginPage from "./login";
 
 export default function Home() {
+  const session = useSession();
+  console.log(session);
+  if (!session) return (<LoginPage />)
   return (
     <div className="flex mt-4 max-w-4xl mx-auto gap-6">
       <div className="w-1/3" >
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,11 +1,14 @@
 
 import { Card } from "@/Components/card";
-import { Layout } from "@/Components/profilePage/layout";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import Link from "next/link";
 
 
 export default function LoginPage() {
-
+    const supabase = useSupabaseClient();
+    async function loginWithGoogle() {
+        await supabase.auth.signInWithOAuth({ provider: "google" });
+    }
     return (
 
         <div className="h-screen flex items-center">
@@ -13,7 +16,7 @@ export default function LoginPage() {
                 <h1 className="text-6xl mb-4 text-gray-300 text-center">Login</h1>
                 <Card>
                     <div className="rounded-md">
-                        <button className="flex w-full gap-4 items-center justify-center p-4 border-b border-b-gray-100 hover:bg-socialBlue hover:text-white hover:border-b-socialBlue hover:rounded-md hover:shadow-md hover:shadow-gray-300 transition-all hover:scale-110">
+                        <button onClick={loginWithGoogle} className="flex w-full gap-4 items-center justify-center p-4 border-b border-b-gray-100 hover:bg-socialBlue hover:text-white hover:border-b-socialBlue hover:rounded-md hover:shadow-md hover:shadow-gray-300 transition-all hover:scale-110">
                             <svg className="h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" /></svg>
                             Login with Google
                         </button>
